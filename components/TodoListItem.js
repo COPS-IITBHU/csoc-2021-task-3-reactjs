5b55afe58890e1d178e62b67009f711f8620527b
raideva <85274247+raideva@users.noreply.github.com>
2021-06-28 23:25:46 +0530

    Update TodoListItem.js

--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -1,7 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
+import axios from "axios";
+import ListItem from "./display";
 
-export default function TodoListItem() {
+
+const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/'
+export default function TodoListItem({todos, settodos}) {
   const editTask = (id) => {
+    document.getElementById('task-' + id).classList.add('hideme');
+    document.getElementById('task-actions-' + id).classList.add('hideme');
+    document.getElementById('input-button-' + id).classList.remove('hideme');
+    document.getElementById('done-button-' + id).classList.remove('hideme');
     /**
      * @todo Complete this function.
      * @todo 1. Update the dom accordingly
@@ -9,14 +17,42 @@ export default function TodoListItem() {
   }
 
   const deleteTask = (id) => {
+    try { iziToast.show({ title: "Wait", message: 'Deleting Todo..' }) } catch { }
+    axios({
+      headers: {
+        Authorization: 'Token ' + localStorage.getItem('token')
+      },
+      url: API_BASE_URL + 'todo/' + id + '/',
+      method: 'delete',
+    }).then(function () { settodos(todos.filter(todo => todo.id != id)); try { iziToast.success({ title: "Success", message: 'Todo Deleted' }) } catch { } }).catch((err) => { try { iziToast.error({ title: "Error", message: 'Cannot Delete Todo' }) } catch { } })
+  }
+
+
     /**
      * @todo Complete this function.
      * @todo 1. Send the request to delete the task to the backend server.
      * @todo 2. Remove the task from the dom.
      */
-  }
+
 
   const updateTask = (id) => {
+    document.getElementById('task-' + id).classList.remove('hideme');
+    document.getElementById('task-actions-' + id).classList.remove('hideme');
+    document.getElementById('input-button-' + id).classList.add('hideme');
+    document.getElementById('done-button-' + id).classList.add('hideme');
+    if (document.getElementById('input-button-' + id).value != '') {
+      try { iziToast.show({ title: "Wait", message: 'Updating Todo..' }) } catch { }
+      document.getElementById('task-' + id).innerHTML = document.getElementById('input-button-' + id).value;
+      const data1 = { "id": id, title: document.getElementById('input-button-' + id).value }
+      axios({
+        headers: { Authorization: 'Token ' + localStorage.getItem('token') },
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'patch',
+        data: {
+          title: document.getElementById('input-button-' + id).value
+        }
+      }).then(() => { settodos(todos.map((todo) => (todo.id === id) ? data1 : todo)); try { iziToast.destroy(); iziToast.success({ title: "Success", message: 'Todo Updated' }) } catch { } }).catch((err) => { try { iziToast.error({ title: "Error", message: 'Cannot Update Todo' }) } catch { } })
+    }
     /**
      * @todo Complete this function.
      * @todo 1. Send the request to update the task to the backend server.
@@ -26,53 +62,11 @@ export default function TodoListItem() {
 
   return (
     <>
-      <li className='border flex border-gray-500 rounded px-2 py-2 justify-between items-center mb-2'>
-        <input
-          id='input-button-1'
-          type='text'
-          className='hideme appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring  todo-edit-task-input'
-          placeholder='Edit The Task'
-        />
-        <div id='done-button-1' className='hideme'>
-          <button
-            className='bg-transparent hover:bg-gray-500 text-gray-700 text-sm  hover:text-white py-2 px-3 border border-gray-500 hover:border-transparent rounded todo-update-task'
-            type='button'
-            onClick={updateTask(1)}
-          >
-            Done
-          </button>
-        </div>
-        <div id='task-1' className='todo-task  text-gray-600'>
-          Sample Task 1
-        </div>
-        <span id='task-actions-1' className=''>
-          <button
-            style={{ marginRight: '5px' }}
-            type='button'
-            onClick={editTask(1)}
-            className='bg-transparent hover:bg-yellow-500 hover:text-white border border-yellow-500 hover:border-transparent rounded px-2 py-2'
-          >
-            <img
-              src='https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png'
-              width='18px'
-              height='20px'
-              alt='Edit'
-            />
-          </button>
-          <button
-            type='button'
-            className='bg-transparent hover:bg-red-500 hover:text-white border border-red-500 hover:border-transparent rounded px-2 py-2'
-            onClick={deleteTask(1)}
-          >
-            <img
-              src='https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg'
-              width='18px'
-              height='22px'
-              alt='Delete'
-            />
-          </button>
-        </span>
-      </li>
+     {
+        todos.map((user) => (
+          <ListItem updateTask={() => updateTask(user.id)} deleteTask={() => deleteTask(user.id)} editTask={() => editTask(user.id)} iid={user.id} title={user.title} key={user.id} />
+        ))
+      }
     </>
   )
 }
