e225747e4a0e58e92dbe398264c39c2230e3c119
raideva <85274247+raideva@users.noreply.github.com>
2021-06-28 23:20:12 +0530

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,12 @@
+import Nav from '../components/Nav'
 import RegisterForm from '../components/RegisterForm'
+import { NoAuthRequired } from '../middlewares/no_auth_required'
 
 export default function Register() {
+  NoAuthRequired();
   return (
     <div>
+      <Nav />
       <RegisterForm />
     </div>
   )
