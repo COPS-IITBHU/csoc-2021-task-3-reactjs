7a2dbe399d74c7983d8107d0d6cbac8197d8527a
raideva <85274247+raideva@users.noreply.github.com>
2021-06-28 23:19:38 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,12 @@
+import Nav from '../components/Nav'
 import LoginForm from '../components/LoginForm'
+import { NoAuthRequired } from '../middlewares/no_auth_required'
 
 export default function Login() {
+  NoAuthRequired();
   return (
     <div>
+      <Nav />
       <LoginForm />
     </div>
   )
