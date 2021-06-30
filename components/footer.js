4e7750e8d7bc9cac0a33615a27df233131f0d32e
Naveen Kumar <naveenkumar12062002@gmail.com>
2021-06-30 15:22:10 +0530

    V 1.0.8

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+// importing... 
+
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useAppContext } from "../context/AppContext";
+
+
+
+// function for the footer 
+export default function Footer()
+{
+    return <>
+        {/* Plan Got cancelled  */}
+    </>
+}
---
+++ pages/forgotPassword.js
@@ -0,0 +1,17 @@
+// importing... 
+
+import Nav from "../components/Nav";
+import LoginForm from "../components/LoginForm";
+import Script from "next/script";
+import { useNoAuthRequired } from "../middlewares/no_auth_required";
+
+
+
+
+export default function forgotDetails(){
+    return <>
+
+        {/* implemented by other way in the loginForm.js itself  */}
+
+    </>
+}
